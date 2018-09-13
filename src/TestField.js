c3c756be1ca82aa79225140dd6aea5964709b7a6
fan CDA <fanCDApozdrawia@gmail.com>
2018-09-14 00:31:59 +0100

    Nothing fancy in here, just extracting the data from the component and generating in the loops (instead of manually).

--- src/TestField.js
+++ src/TestField.js
@@ -2,24 +2,52 @@ import React, { Component } from 'react';
 import Box from './Box';
 import DraggableBox from './DraggableBox';
 
+const DATA = [
+  { text: 'Box 1', bgColor: 'red' },
+  { text: 'Box 2', bgColor: 'green' },
+  { text: 'Box 3', bgColor: 'blue' },
+  { text: 'Box 4', bgColor: 'yellow' },
+];
+
 class TestField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: DATA
+    }
+  }
+
   render() {
     return (
       <div>
         <div style={{ display: 'flex' }}>
-          <DraggableBox text='Box 1' bgColor='red' />
-          <DraggableBox text='Box 2' bgColor='green' />
-          <DraggableBox text='Box 3' bgColor='blue' />
-          <DraggableBox text='Box 4' bgColor='yellow' />
+          {this.generateDraggableBoxes()}
         </div>
         <div style={{ display: 'flex' }}>
-          <Box text='Box 1' bgColor='red' />
-          <Box text='Box 2' bgColor='green' />
-          <Box text='Box 3' bgColor='blue' />
-          <Box text='Box 4' bgColor='yellow' />
+          {this.generateBoxes()}
         </div>
       </div>
     );
+  }
+
+  generateDraggableBoxes() {
+    return this.state.data.map(({ text, bgColor }, index) => (
+      <DraggableBox
+        key={index}
+        text={text}
+        bgColor={bgColor}
+      />
+    ));
+  }
+
+  generateBoxes() {
+    return this.state.data.map(({ text, bgColor }, index) => (
+      <Box
+        key={index}
+        text={text}
+        bgColor={bgColor}
+      />
+    ));
   }
 }
 
