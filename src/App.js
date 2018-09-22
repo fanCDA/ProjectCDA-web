37dc7789d33252c81e67331d452838b42df5bbd3
fan CDA <fanCDApozdrawia@gmail.com>
2018-09-22 21:03:48 +0100

    Trying to do drag and drop without third party libraries.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const DATA = [
+  'A',
+  'B',
+  'C',
+  'D',
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="columns">
+        {DATA.map((value, index) => {
+          return (
+            <div
+              className="column"
+              draggable="true"
+              key={index}
+              onDragStart={e => this.handleDragStart(e)}
+              onDragEnter={e => this.handleDragEnter(e)}
+              onDragOver={e => this.handleDragOver(e)}
+              onDragLeave={e => this.handleDragLeave(e)}
+              onDrop={e => this.handleOnDrop(e)}
+              onDragEnd={e => this.handleOnDragEnd(e)}
+            >
+              <header>{value}</header>
+            </div>
+          )
+        })}
       </div>
     );
+  }
+
+  handleDragStart(e) {
+    // this / e.target is the source node.
+    console.log('handleDragStart');
+    // console.log(e);
+
+    e.target.style.opacity = 0.4;
+  }
+
+  handleDragEnter(e) {
+    // this / e.target is the current hover target.
+    console.log('handleDragEnter');
+    // console.log(e);
+
+    e.target.classList.add('over');
+  }
+
+  handleDragOver(e) {
+    console.log('handleDragOver');
+    // console.log(e);
+
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+    return false;
+  }
+
+  handleDragLeave(e) {
+    // this / e.target is previous target element.
+    console.log('handleDragLeave');
+    // console.log(e);
+
+    e.target.classList.remove('over');
+  }
+
+  handleOnDrop(e) {
+    // this / e.target is current target element.
+    console.log('handleOnDrop');
+    // console.log(e);
+
+    e.preventDefault();
+    e.target.classList.remove('over');
+    return false;
+  }
+
+  handleOnDragEnd(e) {
+    // this/e.target is the source node.
+    console.log('handleOnDragEnd');
+    // console.log(e);
+
+    e.target.style.opacity = 1;
   }
 }
 
