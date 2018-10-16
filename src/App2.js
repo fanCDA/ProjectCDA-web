f8f093c34579e4b2030adbf23020dc653eeac586
fan CDA <fanCDApozdrawia@gmail.com>
2018-10-16 14:05:18 +0200

    Dividing each page to 2 halves.

--- src/App2.js
+++ src/App2.js
@@ -5,13 +5,22 @@ class App extends Component {
   render() {
     let items = [];
     for(let i=0; i<50; i++) {
-      items.push(
-        <div className="grid-item" key={i}>{i+1}</div>
-      );
+      items.push( this.getItem(i) );
     }
     return (
       <div className="grid-container">
         {items}
+      </div>
+    );
+  }
+
+  getItem(i) {
+    return (
+      <div className="grid-item" key={i}>
+        <div className="grid-container-2">
+          <div className="grid-item-2">L</div>
+          <div className="grid-item-2">R</div>
+        </div>
       </div>
     );
   }
