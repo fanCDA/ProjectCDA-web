369f8131575e23a487fba27a1d861e076c1985b7
fan CDA <fanCDApozdrawia@gmail.com>
2018-10-18 23:17:37 +0100

    Creating few variations of single page, so we can split it into few different parts with different ratios.

--- src/App2.js
+++ src/App2.js
@@ -18,11 +18,76 @@ class App extends Component {
     return (
       <div className="grid-item" key={i}>
         <div className="grid-container-2">
-          <div className="grid-item-2">L</div>
-          <div className="grid-item-2">R</div>
+          <div className="grid-item-2">
+            {this.getSinglePage(i)}
+          </div>
+          <div className="grid-item-2">
+            R
+          </div>
         </div>
       </div>
     );
+  }
+
+  getSinglePage(i) {
+    if(i === 1) {
+      return (
+        <div className="single-page-container">
+          <div style={{flex: 1, backgroundColor: 'green'}}>1</div>
+          <div style={{flex: 1, backgroundColor: 'yellow'}}>2</div>
+        </div>
+      );
+    } else if(i === 2) {
+      return (
+        <div className="single-page-container">
+          <div style={{flex: 1, backgroundColor: 'green'}}>1</div>
+          <div style={{flex: 1, backgroundColor: 'yellow'}}>2</div>
+          <div style={{flex: 1, backgroundColor: 'pink'}}>3</div>
+        </div>
+      );
+    } else if(i === 3) {
+      return (
+        <div className="single-page-container">
+          <div style={{flex: 1, backgroundColor: 'green'}}>1</div>
+          <div style={{flex: 1, backgroundColor: 'yellow'}}>2</div>
+          <div style={{flex: 1, backgroundColor: 'pink'}}>3</div>
+          <div style={{flex: 1, backgroundColor: 'brown'}}>4</div>
+        </div>
+      );
+    } else if(i === 7) {
+      return (
+        <div className="single-page-container">
+          <div style={{flex: 1, backgroundColor: 'green'}}>1</div>
+          <div style={{flex: 2, backgroundColor: 'yellow'}}>2</div>
+        </div>
+      );
+    } else if(i === 8) {
+      return (
+        <div className="single-page-container">
+          <div style={{flex: 2, backgroundColor: 'green'}}>1</div>
+          <div style={{flex: 1, backgroundColor: 'yellow'}}>2</div>
+          <div style={{flex: 1, backgroundColor: 'pink'}}>3</div>
+        </div>
+      );
+    } else if(i === 9) {
+      return (
+        <div className="single-page-container">
+          <div style={{flex: 1, backgroundColor: 'green'}}>1</div>
+          <div style={{flex: 2, backgroundColor: 'yellow'}}>2</div>
+          <div style={{flex: 1, backgroundColor: 'pink'}}>3</div>
+        </div>
+      );
+    } else if(i === 10) {
+      return (
+        <div className="single-page-container">
+          <div style={{flex: 2, backgroundColor: 'green'}}>1</div>
+          <div style={{flex: 1, backgroundColor: 'yellow'}}>2</div>
+          <div style={{flex: 2, backgroundColor: 'pink'}}>3</div>
+        </div>
+      );
+    }
+    
+    return <div>L</div>;
   }
 }
 
